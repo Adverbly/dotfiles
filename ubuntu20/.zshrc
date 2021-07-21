8602b9dbfa4332a777136d8649d0b23e63211b78
George Montgomery <gmcmonty@gmail.com>
2021-07-21 10:54:07 -0400

    update

--- ubuntu20/.zshrc
+++ ubuntu20/.zshrc
@@ -130,6 +130,8 @@ fi
 autoload -Uz compinit && compinit -i
 eval "$(rbenv init -)"
 
+source $HOME/.gvm/scripts/gvm
+
 export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
@@ -138,3 +140,8 @@ export NVM_DIR="$HOME/.nvm"
 [[ ! -f ~/.p10k.zsh ]] || source ~/.p10k.zsh
 
 [ -f ~/.fzf.zsh ] && source ~/.fzf.zsh
+source /usr/share/doc/fzf/examples/key-bindings.zsh
+source /usr/share/doc/fzf/examples/completion.zsh
+
+eval "$(direnv hook zsh)"
+
--- ubuntu20/install.sh
+++ ubuntu20/install.sh
@@ -18,6 +18,33 @@ sudo apt-get install -y v4l2loopback-dkms
 sudo add-apt-repository ppa:obsproject/obs-studio
 sudo apt update
 sudo apt install -y obs-studio
+sudo apt install -y libpq-dev
+sudo apt install -y direnv
+sudo apt-get install -y libcurl3-dev
+sudo apt-get install -y \
+    apt-transport-https \
+    ca-certificates \
+    curl \
+    gnupg \
+    lsb-release
+sudo apt install -y libnss3-tools
+sudo apt install -y hub
+
+
+# install docker
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+echo \
+  "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
+  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt-get update
+sudo apt-get install docker-ce docker-ce-cli containerd.io
+sudo groupadd docker
+sudo usermod -aG docker $USER
+newgrp docker
+
+# compose
+sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
 
 
 # install neovim
@@ -70,6 +97,12 @@ gvm install go1.16
 
 # install rubymine
 ## manual?
+# install atom icons
+# install material theme + configure it
+# tweak fonts, appearance
+# add external tools
+npm install --global prettier @prettier/plugin-ruby
+# create external tool that runs program: prettier, arguments: --write $FilePath$
 
 # symlink saved config
 mv $HOME/.zshrc $HOME/.zshrc-old
@@ -78,4 +111,8 @@ mv $HOME/.tmux.conf.local $HOME/.tmux.conf.local-old
 ln -s "$(pwd)/.tmux.conf.local" $HOME/.tmux.conf.local
 mkdir -p $HOME/.config/nvim
 ln -s "$(pwd)/init.vim" $HOME/.config/nvim/init.vim
+
+
+# increase file watcher limit
+echo fs.inotify.max_user_watches=524288 | sudo tee -a /etc/sysctl.conf && sudo sysctl -p
 
